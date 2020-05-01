940ab998cec3144b64ecb58f91c35a5fc25417a7
Mark Albrand <62487869+markalbrand56@users.noreply.github.com>
2020-04-30 22:45:10 -0600

    Changes to convert to exe file

--- FDS.py
+++ FDS.py
@@ -5,31 +5,39 @@ import collections
 from s_package.interval import finterval
 
 def frecuencias():
-    data = []
-    counter = 0
-    while True:
-        var = float(input("Enter a number: "))
-        if var == 0.0:
-            break #0 is for finishing the input process
+    ts = True
+    while ts:
+        data = []
+        counter = 0
+        while True:
+            var = float(input("Enter a number: "))
+            if var == 0.0:
+                break #0 is for finishing the input process
+            else:
+                counter += 1
+                data.append(var)
+        data = sorted(data)
+        countrep = collections.Counter(data) #this will sort data by how many times it appears in the data collection
+        i_range = max(data) - min(data)
+        min_inter = int(input("How many intervals do you at least need? "))
+        max_inter = int(input("How many intervals is your max? "))
+        max_inter += 1
+        intervals = finterval(i_range,min_inter,max_inter)
+        print("\n-------")
+        print ("Sorted data: ", data)
+        print("Amount of data: ", counter)
+        print ("Data, and how many times they repeat: ",countrep)
+        if intervals == 0:
+            print("An error ocurred while calculating intervals...")
         else:
-            counter += 1
-            data.append(var)
-    data = sorted(data)
-    countrep = collections.Counter(data) #this will sort data by how many times it appears in the data collection
-    i_range = max(data) - min(data)
-    min_inter = int(input("How many intervals do you at least need? "))
-    max_inter = int(input("How many intervals is your max? "))
-    max_inter += 1
-    intervals = finterval(i_range,min_inter,max_inter)
-    print("\n-------")
-    print ("Sorted data: ", data)
-    print("Amount of data: ", counter)
-    print ("Data, and how many times they repeat: ",countrep)
-    if intervals == 0:
-        print("An error ocurred while calculating intervals...")
-    else:
-        print("Possible intervals: ",intervals)
-    print("-------")
+            print("Possible intervals: ",intervals)
+        print("-------")
+        exiting = input("Do you want to exit? y/n ")
+        if exiting == 'y':
+            ts = False
+            exit()
+        else:
+            pass
 
 if __name__ == "__main__":
     frecuencias()
