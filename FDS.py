683d2cb0b8cd15d8dd454fea0e4f61bd2766a348
Mark Albrand <62487869+markalbrand56@users.noreply.github.com>
2020-05-11 20:42:39 -0600

    Updated comments

--- FDS.py
+++ FDS.py
@@ -23,37 +23,37 @@ def frecuencias():
                     counter += 1
                     data.append(var)
             except ValueError:
-                print(" Enter a number please.") #Ecen though this exception is raised, the user can continue to try to enter data
+                print(" Enter a number please.") #Even though this exception is raised, the user can continue to try to enter data
         data = sorted(data)
         if len(data) <= 1:
             print("\nEnter more than one number, please. Try again.")
             print("Exiting...")
             break
-        ##reg_freq = collections.Counter(data) #this will sort data by how many times it appears in the data collection
+
         max_data = max(data)
         min_data = min(data)
-        i_range = max_data - min_data
+        i_range = max_data - min_data #Range
         min_inter = int(input("\n  How many classes/groups do you at least need? "))
         max_inter = int(input("  How many classes/groups is your max? "))
-        max_inter += 1
-        p_group_sizes = finterval(i_range,min_inter,max_inter)
+        max_inter += 1 #This makes the max number of intervals actually be considered in the range.
+        p_group_sizes = finterval(i_range,min_inter,max_inter) #Possible group sizes
         
         print("\n\n\n\n-----------------------------------------------------------------")
         print ("Sorted data: ", data)
         print("Amount of data: ", counter)
         print("\nRange: ", i_range)
-        frequency(data)
-        ##rint ("Data, and how many times they repeat: ",reg_freq)
+        frequency(data) #this will sort data by how many times it appears in the data collection
+
         print("-----------------------------------------------------------------")
         if p_group_sizes == 0:
             print("An error ocurred while calculating intervals...")
-            exit()
+            exit() #Without a possible group size it cannot continue with the calculations
         else:
             for i in range(len(p_group_sizes)):
                 print("\n",i+1,". Possible group size: {} groups".format(p_group_sizes[i][0]),"  This was {} divided by".format(i_range), p_group_sizes[i][1])
         print("-------")
         
-        chs_interval = int(input("  Choose a group size: ")) #Options to choose
+        chs_interval = int(input("  Choose a group size: ")) #Options to choose the width of the gruops
         chs_interval -= 1
         if p_group_sizes[chs_interval]:
             pass
@@ -64,7 +64,7 @@ def frecuencias():
         class_int = int(p_group_sizes[chs_interval][1])-1
         print("Group's width: ", class_int, "\n")
         
-        f_limits = limits(min=min_data,max=max_data,clsi=class_int) #returns list with the limits. This has to change to a dict, with the frequency
+        f_limits = limits(min=min_data,max=max_data,clsi=class_int) #returns list with the limits.
         result_gr_f = group_frequencies(f_limits, data)
         acc_g_f = 0
         
@@ -73,11 +73,7 @@ def frecuencias():
             acc_g_f += freq[1]
             print (freq[0], "  |  ", freq[1], "   |   ", acc_g_f)
         print ("---------------------------Accumulated frequency: ", acc_g_f)
-        '''
-        print("Your intervals are:\n ")
-        for limit in f_limits:
-            print (" ", limit) #Here will have to appear the frequency por each group
-'''
+    
         if len(f_limits) == chosen_group_size:
             pass
         elif len(f_limits) > chosen_group_size:
