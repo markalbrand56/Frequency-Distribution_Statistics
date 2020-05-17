d65c698acc13bca5f2f084ac2efd9ab9829c7b00
Mark Albrand <62487869+markalbrand56@users.noreply.github.com>
2020-05-17 13:41:05 -0600

    Terms /documentation for interval.py

--- s_package/interval.py
+++ s_package/interval.py
@@ -1,22 +1,22 @@
 def finterval(rang,min=8,max=19):
-    poss_inter = []
-    inter_lst = []
+    possible_ammounts_groups = []
+    results = []
     for x in range (min,max):
-                poss_inter.append(x)
-                if x in poss_inter:
+                possible_ammounts_groups.append(x)
+                if x in possible_ammounts_groups:
                     pass
     for num_groups in range(2,(int(rang))): #Divisor and class' interval. Starts from 2 because the divisor needs to get substarcted 1 calculate groups' width
-        if rang / num_groups in poss_inter:
+        if rang / num_groups in possible_ammounts_groups:
             groups_width = rang / num_groups 
-            inter_lst.append((groups_width, num_groups)) 
-    if len(inter_lst) == 0:
+            results.append((groups_width, num_groups)) 
+    if len(results) == 0:
         return 0 #Instead of an empty list. Easier to check
     else:
-        return inter_lst
+        return results
 
 
 if __name__ == "__main__":
-    print(finterval(56), "Test") #testing
+    print(finterval(24), "Test") #testing
 
 
     
