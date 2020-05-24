f1d84b1cb59b274fb60503423b4c4d95b5690eca
Mark Albrand <62487869+markalbrand56@users.noreply.github.com>
2020-05-24 16:48:15 -0600

    Exiting and timer (for exe file in mind)

--- s_package/exiting.py
+++ s_package/exiting.py
@@ -1,9 +1,11 @@
 def Exiting():
+    import time
     while True:
         exiting = input("   Do you want to exit? y/n ")
         if exiting == "y" or exiting == "Y":
+            time.sleep(1)
             tr = False
-            break
+            exit()
         else:
             print("   Ok, when you're ready exit by pressing 'CTRL'+'C' ")
             while True:
