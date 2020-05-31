14507df5bb327a9eec81424890db7cf6641e8d92
Mark Albrand <62487869+markalbrand56@users.noreply.github.com>
2020-05-30 20:52:35 -0600

    Limits and frequencies input

--- FDS_R.py
+++ FDS_R.py
@@ -4,6 +4,37 @@ This means that the user should also have every grouped frequency.
 The program will calculate class centers, and the arithmetic average of the table (using the formula and deviation)
 """
 
+from s_package.get_rest_groups import Get_other_groups
+from s_package.exiting import Exiting
+
 
 def arithmetic_average():
-    pass
+    input_loop = True
+    frequencies_loop = True
+
+    while input_loop:
+        try:
+            first_lower = int(input("Whats the lower limit of the first group? "))
+            first_upper = int(input("Whats the upper limit of the first group? "))
+            amount_groups = int(input("How many groups does the table have? "))
+            input_loop = False
+        except ValueError:
+            print("Enter a number, please")
+    limits = Get_other_groups(first_lower, first_upper, amount_groups)
+    print(limits)
+
+    groups_frequencies = []
+    for limit in limits:
+        try:
+            frequency = int(input("Enter the frequency for {} : ".format(limit)))
+            group_freq = (limit, frequency)
+            groups_frequencies.append(group_freq)
+        except ValueError:
+            print("Enter a number, please")
+
+    print(groups_frequencies)
+
+
+
+if __name__ == '__main__':
+    arithmetic_average()
---
+++ s_package/get_rest_groups.py
@@ -0,0 +1,21 @@
+def Get_other_groups(lower_limit, upper_limit, amount_groups):
+    """
+    One group is needed to calculate the rest. It's also needed the amount of groups to create
+    """
+    width = upper_limit - lower_limit
+    lower = lower_limit
+    upper = upper_limit
+    limits = []
+    for i in range(amount_groups):
+        limit = (lower, upper)
+        limits.append(limit)
+        lower = lower + width + 1
+        upper = upper + width + 1
+    return limits
+
+
+if __name__ == '__main__':
+    try:
+        print(Get_other_groups(60, 69, 6))
+    except:
+        print("error")
