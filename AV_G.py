36f23f78402f19d50110526f59dc5fb7979d3459
Mark Albrand <62487869+markalbrand56@users.noreply.github.com>
2020-06-02 22:06:29 -0600

    Deviation working also for groups. Code needs cleanup

--- AV_G.py
+++ AV_G.py
@@ -18,6 +18,7 @@ def arithmetic_averages_grouped_freq():
         try:
             first_lower = int(input("Whats the lower limit of the first group? "))
             first_upper = int(input("Whats the upper limit of the first group? "))
+            width = first_upper - first_lower + 1
             amount_groups = int(input("How many groups does the table have? "))
             input_loop = False
         except ValueError:
@@ -52,6 +53,42 @@ def arithmetic_averages_grouped_freq():
     print("Your arithmetic average is: {}".format(a_average))
 
     # TODO Arithmetic average with deviation
+    frequency_total = 0
+    for element in groups_frequencies:
+        freq = element[1]
+        frequency_total += freq
+
+    dev_loop = True
+    while dev_loop:
+        try:
+            deviation_position = int(input("In what position do you want to set the center of the deviation? "))
+            deviation_position -= 1
+            dev_loop = False
+        except ValueError:
+            print("Enter a number please")
+
+    deviations = []
+    for num in range(len(groups_frequencies)):
+        value = groups_frequencies[num][1]  # limit
+        d = int(num - deviation_position)
+        deviation = (value, d)
+        deviations.append(deviation)
+
+    print(deviations)
+
+    deviations_subtotal = 0
+    for element in deviations:
+        f = element[0]
+        d = element[1]
+        result = f * d
+        deviations_subtotal += result
+
+    print(deviations_subtotal)
+    print(frequency_total)
+    print(class_centers[deviation_position])
+    deviations_result = class_centers[deviation_position] + (deviations_subtotal / frequency_total) * width
+    deviations_result = round(deviations_result, 2)
+    print(deviations_result)
 
 
 if __name__ == '__main__':
