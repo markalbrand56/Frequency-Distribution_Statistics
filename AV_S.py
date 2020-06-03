31f12b836021eae5044bda943bc5b08bbf91ca9c
Mark Albrand <62487869+markalbrand56@users.noreply.github.com>
2020-06-02 21:46:09 -0600

    Deviation working. Code needs cleanup

--- AV_S.py
+++ AV_S.py
@@ -13,7 +13,6 @@ def arithmetic_averages_simple_freq():
                 if num == 0.0:
                     break  # 0 is for finishing the input process
                 elif num > 0:
-                    #data_counter += 1
                     data.append(num)
                 else:
                     pass  # Negative numbers won't be added
@@ -37,26 +36,59 @@ def arithmetic_averages_simple_freq():
             except ValueError:
                 print(" \nEnter a number, and try again ")
                 exiting()
-                #main_loop = False
-                #break
+                # main_loop = False
+                # break
         print(data_frequencies)
 
         for element in data_frequencies:
             freq = element[1]
             data_counter += freq
 
-        cumulative_frequency = 0
+        cumulative_data = 0  # the sum of every number
         for element in data_frequencies:
             num = element[0]
             list_frequency = element[1]
             subtotal = num * list_frequency
-            cumulative_frequency += subtotal
+            cumulative_data += subtotal
 
-        print(cumulative_frequency)
+        print(cumulative_data)
 
-        arithmetic_average = cumulative_frequency / data_counter
+        arithmetic_average = cumulative_data / data_counter
         arithmetic_average = round(arithmetic_average, 2)
         print("\nThe arithmetic average is: {}".format(arithmetic_average))
+
+        # TODO Arithmetic averages with deviation
+
+        dev_loop = True
+        while dev_loop:
+            try:
+                deviation_position = int(input("In what position do you want to set the center of the deviation? "))
+                deviation_position -= 1
+                dev_loop = False
+            except ValueError:
+                print("Enter a number please")
+
+        deviations = []
+        for num in range(len(data_frequencies)):
+            value = data_frequencies[num][1]
+            d = int(num - deviation_position)
+            deviation = (value, d)
+            deviations.append(deviation)
+
+        print(deviations)
+
+        deviations_subtotal = 0
+        for element in deviations:
+            f = element[0]
+            d = element[1]
+            result = f * d
+            deviations_subtotal += result
+
+        print(deviations_subtotal)
+        print(cumulative_data)
+        deviations_result = data_frequencies[deviation_position][0] + (deviations_subtotal / data_counter)
+        deviations_result = round(deviations_result, 2)
+        print(deviations_result)
 
         main_loop = False
 
